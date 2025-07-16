d4befc3a33af91176a625c3ef29137918f1be850
Wreinam <renan_etec@hotmail.com>
2025-07-16 18:49:10 -0300

    Login/Register e outras navegações de teste

--- app/Http/Controllers/authentications/RegisterBasic.php
+++ app/Http/Controllers/authentications/RegisterBasic.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers\authentications;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterBasic extends Controller
 {
   public function index()
   {
     return view('content.authentications.auth-register-basic');
+  }
+
+  public function store(Request $request)
+  {
+    $request->validate([
+      'name'     => 'required|string|max:255',
+      'email'    => 'required|email|unique:users,email',
+      'password' => 'required|string|min:6|confirmed', // precisa do campo password_confirmation
+    ]);
+
+    User::create([
+      'name'     => $request->name,
+      'email'    => $request->email,
+      'password' => $request->password, // Laravel vai hashear automaticamente
+    ]);
+
+    return redirect()->route('auth-login-basic')->with('success', 'Usuário registrado com sucesso!');
   }
 }
--- config/variables.php
+++ config/variables.php
@@ -3,7 +3,7 @@
   return [
     "creatorName" => "ThemeSelection",
     "creatorUrl" => "https://themeselection.com",
-    "templateName" => "sneat",
+    "projectName" => "Hórus",
     "templateSuffix" => "Bootstrap Dashboard FREE",
     "templateVersion" => "2.0.0",
     "templateFree" => true,
